598604c22dd02ccc9a1b2191fcb693cf29ce9c87
TianYaKe <liulong@hzins.com>
2016-04-18 23:06:11 +0800

    Initial Commit

---
+++ YYCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYCacheDemo
+//
+//  Created by Alen on 16/4/18.
+//  Copyright © 2016年 JingKeCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYCacheDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYCacheDemo
+//
+//  Created by Alen on 16/4/18.
+//  Copyright © 2016年 JingKeCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
